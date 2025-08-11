8037992b7ea3d5800bc28b0655ae151cbb79516b
Qusai Abudhaim <Qusai.Abudhaim@gmail.com>
2025-08-11 02:15:46 +0200

    Build the frontend

--- Server/Metrics/urls.py
+++ Server/Metrics/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index
+from .views import  index, show_device
 from django.conf.urls.static import static
 from .apiViews import DeviceListCreateView, SystemMetricsListCreateView, NetworkMetricsListCreateView, DeviceDetailView
 from django.conf import settings
@@ -7,6 +7,7 @@ from django.conf import settings
 app_name = "Metrics"
 urlpatterns = [
     path("", index, name="index"),
+    path('show_device/<str:device_id>/', show_device, name='show_device'),
     path("api/system-metrics/", SystemMetricsListCreateView.as_view(), name="system_metrics"),
     path("api/network-metrics/", NetworkMetricsListCreateView.as_view(), name="network_metrics"),
     path("api/devices/", DeviceListCreateView.as_view(), name="devices"),
--- Server/Metrics/views.py
+++ Server/Metrics/views.py
@@ -1,19 +1,42 @@
 from django.shortcuts import render
+from django.db.models import Q
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Device
 
 # Create your views here.
 # make an index file
 
 def index(request):
-    """
-    Render the index page for the Metrics application.
+    """Render the index page for the Metrics application."""
+
+    query = request.GET.get('query', '')
+
+    devices_list = Device.objects.filter(Q(device_id__icontains=query)).order_by('-created_at')
+
+    page = request.GET.get('page', 1)
+    paginator = Paginator(devices_list, 8)
+
+    try:
+        devices = paginator.page(page)
+    except PageNotAnInteger:
+        devices = paginator.page(1)
+    except EmptyPage:
+        devices = paginator.page(paginator.num_pages)
+
+    context = {
+        'Devices': devices,
+    }
     
-    Args:
-        request: The HTTP request object.
+    return render(request, 'index.html', context)
+
+def show_device(request, device_id):
+    """Render the page for a specific device."""
+
+    context = {
+        'device_id': device_id,
+    }
     
-    Returns:
-        HttpResponse: Rendered index page.
-    """
-    return render(request, 'index.html')
+    return render(request, 'show_device.html', context)
 
 
 def handler500(request, *args, **argv):
